80e423c90803ce40bab3eecca91fb91b87e6005a
kaka <huangjc7@foxmail.com>
2023-12-24 01:52:52 +0800

    新增readme文件

--- mergeSort/mergeSort_test.go
+++ mergeSort/mergeSort_test.go
@@ -6,11 +6,37 @@ import (
 )
 
 func TestMergeSort(t *testing.T) {
-	arr := []int{4, 3, 2, 1}
-	want := []int{1, 2, 3, 4}
-
-	MergeSort(arr, len(arr))
-	if !reflect.DeepEqual(arr, want) {
-		t.Errorf("MergeSort() = %v, want %v", arr, want)
+	tests := []struct {
+		name string
+		arr  []int
+		want []int
+	}{
+		{
+			name: "排序测试",
+			arr:  []int{4, 3, 2, 1},
+			want: []int{1, 2, 3, 4},
+		},
+		{
+			name: "空数组测试",
+			arr:  []int{},
+			want: []int{},
+		},
+		{
+			name: "单个数字测试",
+			arr:  []int{1},
+			want: []int{1},
+		},
+		{
+			name: "顺序测试",
+			arr:  []int{1, 2, 3, 4},
+			want: []int{1, 2, 3, 4},
+		},
 	}
+	for _, v := range tests {
+		MergeSort(v.arr, len(v.arr))
+		if !reflect.DeepEqual(v.arr, v.want) {
+			t.Errorf("MergeSort() = %v, want %v", v.arr, v.want)
+		}
+	}
+
 }
