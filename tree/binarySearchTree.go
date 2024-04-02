7ee13314d2a61eaf17ab0d2b1649a98c4c26798d
jianchen huang <huangjc7@foxmail.com>
2024-04-03 01:54:50 +0800

    新增算法练习代码

--- tree/binarySearchTree.go
+++ tree/binarySearchTree.go
@@ -24,6 +24,22 @@ func inOrderTraversal(root *TreeNode) {
 	}
 }
 
+func preOrderTraversal(root *TreeNode) {
+	if root != nil {
+		fmt.Println(root.Val)
+		preOrderTraversal(root.Left)
+		preOrderTraversal(root.Right)
+	}
+}
+
+func postOrderTraversal(root *TreeNode) {
+	if root != nil {
+		postOrderTraversal(root.Left)
+		postOrderTraversal(root.Right)
+		fmt.Println(root.Val)
+	}
+}
+
 func (bst *BinarySearchTree) insert(data int) {
 	if bst.tree == nil {
 		bst.tree = &TreeNode{Val: data}
@@ -52,11 +68,8 @@ func (bst *BinarySearchTree) insert(data int) {
 }
 
 func (bst *BinarySearchTree) find(data int) bool {
-	if bst.tree == nil {
-		return false
-	}
 	p := bst.tree
-	for {
+	for p != nil {
 		if p.Val == data {
 			fmt.Println("value has been found", p.Val)
 			return true
@@ -70,4 +83,63 @@ func (bst *BinarySearchTree) find(data int) bool {
 		}
 	}
 	return false
+}
+
+func (bst *BinarySearchTree) delete(data int) {
+	// record current node
+	p := bst.tree
+	// record current node father
+	var pp *TreeNode
+
+	//      4
+	//     / \
+	//    2   6
+	//   / \ /
+	//  1  3 5
+
+	for p != nil {
+		// judge leaf node
+		if data < p.Val {
+			pp = p
+			p = p.Left
+
+		} else if data > p.Val {
+			pp = p
+			p = p.Right
+			// must is leaf node
+
+		} else {
+			if p.Left != nil && p.Right != nil { // Node has two children
+				// Find the minimum node on the right subtree
+				minP := p.Right
+				minPP := p // parent of minP
+				for minP.Left != nil {
+					minPP = minP
+					minP = minP.Left
+				}
+				p.Val = minP.Val // Replace value
+				p = minP         // Move to minP to delete it
+				pp = minPP       // Let the parent node pointer of the initially deleted element point to the new node
+			}
+
+			//node 0 or 1 child
+			var child *TreeNode
+			if p.Left != nil {
+				child = p.Left
+			} else if p.Right != nil {
+				child = p.Right
+			}
+
+			// 判断p的pp是不是nil 如果是nil就是root节点
+			if pp == nil {
+				bst.tree = child
+			} else if pp.Left == p {
+				pp.Left = child
+			} else {
+				pp.Right = child
+			}
+			return
+		}
+	}
+	fmt.Println("value not found")
 }
