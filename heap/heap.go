ac40d564264a4c9d4e5a42a5093181425adf4f4c
jianchen huang <huangjc7@foxmail.com>
2024-04-22 21:18:23 +0800

    新增算法练习代码

--- heap/heap.go
+++ heap/heap.go
@@ -1,75 +1,69 @@
-package heap
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Heap struct {
-	arr   []int
+type heap struct {
 	count int
+	a     []int
 }
 
-func newHeap() *Heap {
-
-	return &Heap{
-		arr:   make([]int, 10),
-		count: 0,
+func newHeap(cap int) *heap {
+	return &heap{
+		a: make([]int, cap+1),
 	}
 }
 
-func (h *Heap) insert(data int) {
+func (h *heap) insert(data int) {
 	h.count++
-	h.arr[h.count] = data
+	h.a[h.count] = data
 	i := h.count
 
-	if h.count < 2 {
-		return
-	}
-
-	// 从下往上堆化
-	//i/2 > 0 必须要大于0，如果没有大于0那么就会给index[0]赋值 不满足heap特性 是边界条件之一
-	for i/2 > 0 && h.arr[i] > h.arr[i/2] {
-		//swap
-		h.arr[i], h.arr[i/2] = h.arr[i/2], h.arr[i]
-		//持续往上比较
-		i = i / 2
+	// from down to up heap
+	for h.a[i] > h.a[i>>1] && i>>1 > 0 {
+		h.a[i], h.a[i>>1] = h.a[i>>1], h.a[i]
+		i = i >> 1
 	}
 }
 
-func (h *Heap) removeMax() {
-	h.arr[1] = h.arr[h.count] //最后元素移至堆顶
+func (h *heap) removeMax() {
+	h.a[1] = h.a[h.count]
 	h.count--
-
 	i := 1
-
+	//from up to down heap
 	for {
-		maxPos := i
-		//left
-		if i*2 <= h.count && h.arr[i] < h.arr[i*2] {
-			maxPos = i * 2
+		//largest = 2 i = 2
+		largest := i
+		left := i << 1
+		right := i<<1 + 1
+
+		// compare left
+		if left <= h.count && h.a[largest] < h.a[left] {
+			// largest = 4 i = 2
+			largest = left
 		}
-		//right
-		if i*2+1 <= h.count && h.arr[i] < h.arr[i*2+1] {
-			maxPos = i*2 + 1
+		// compare right
+		if right <= h.count && h.a[largest] < h.a[right] {
+			largest = right
 		}
-		if maxPos == i {
+		if largest == i {
 			break
 		}
-		//swap
-		h.arr[i], h.arr[maxPos] = h.arr[maxPos], h.arr[i]
-		i = maxPos
+		// a[2],a[4] = a[4],a[2]
+		h.a[i], h.a[largest] = h.a[largest], h.a[i]
+		// i = 4 2 = 4
+		i = largest
 	}
 }
 
 func main() {
-	h := newHeap()
-
-	h.insert(5)
-	h.insert(7)
-	h.insert(9)
-	h.insert(100)
-	h.insert(2)
+	h := newHeap(9)
 	h.insert(4)
-	h.insert(19)
+	h.insert(10)
+	h.insert(3)
+	h.insert(5)
 	h.insert(1)
 	h.removeMax()
-	fmt.Println(h.arr)
+	fmt.Println(h.a)
 }
